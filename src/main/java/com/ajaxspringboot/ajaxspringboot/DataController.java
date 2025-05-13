cf12e3bd055949a5dadcc219308c06b1055591bd
Upgrade-PC <07matvej@gmail.com>
2025-05-14 00:36:21 +0300

    Interface fixed

--- src/main/java/com/ajaxspringboot/ajaxspringboot/DataController.java
+++ src/main/java/com/ajaxspringboot/ajaxspringboot/DataController.java
@@ -4,7 +4,7 @@ import java.util.Random;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,13 +13,15 @@ public class DataController {
 
     
      @RequestMapping(value = "/gettable", method = RequestMethod.GET, produces= "text/html")
-     public String getTable() {
+     public String getTable(@RequestParam(value = "width", defaultValue = "5") String width, @RequestParam(value = "height", defaultValue = "5") String height) {
         Random random = new Random();
+        int widthint = Integer.parseInt(width);
+        int heightint = Integer.parseInt(height);
         String records = "<tbody> ";
         
-        for(int i = 1; i <= 5; i++) {
+        for(int i = 1; i <= heightint; i++) {
             records+="<tr data-row='" + i + "'> ";
-            for(int j = 1; j <= 3; j++) {
+            for(int j = 1; j <= widthint; j++) {
                 int x = random.nextInt(-100, 100);
                 records+="<td>" + x + "</td> ";
             }
